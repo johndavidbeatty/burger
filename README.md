cc6f0016ceaedeb6664cc2d81544efd06d8002c0
John Beatty <gitboot@jbeatty.com>
2018-08-19 00:22:09 -0400

    first upload of code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # burger
+
+This is my burger homework, currently it just deletes the burger instead of moving.
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burger_name: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, false
+  ], function(result) {
+    // Send back the ID of the new burger
+    res.json({ id: result.insertId });
+  });
+});
+
+
+
+router.delete("/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
