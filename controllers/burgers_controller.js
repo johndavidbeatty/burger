85b9d2284ddf359885802e67709c63175554bc54
John Beatty <gitboot@jbeatty.com>
2018-08-19 01:58:18 -0400

    changed to devoured status, added trashed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,11 +29,30 @@ router.post("/burgers", function(req, res) {
 
 
 
-router.delete("/burgers/:id", function(req, res) {
+router.delete("/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.post("/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  var eaten = true;
+
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: eaten
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
